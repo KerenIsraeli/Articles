17cc7ad48fba653f031ddebd1db49c4478734f81
Yairgadiel <Yairgadiel@gmail.com>
2023-07-21 11:55:53 +0300

    removed everything

--- app/src/main/java/com/keren/articles/domain/ArticlesRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.keren.articles.domain
-
-import com.keren.articles.customComponents.Resource
-import com.keren.articles.data.Articles
-
-interface ArticlesRepository {
-
-    suspend fun getArticles(): Resource<Articles>
-}
